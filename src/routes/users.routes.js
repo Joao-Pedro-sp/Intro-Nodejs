c7659d6416376819e0fa808d668fb369e09fe8dc
joao <jp_santana00@outlook.com>
2022-11-24 15:16:09 -0300

    Routes

---
+++ src/routes/users.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require("express")
+
+const usersRoutes = Router()
+
+usersRoutes.post("/", (request, response) => {
+  const { name, email, password } = request.body;
+
+
+  response.send(`Usuario: ${name}. - E-mail: ${email}. E a senha e: ${password}`)
+})
+
+module.export = usersRoutes;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
 const express = require('express')
 
+const routes = require("./routes")
+
 const app = express()
 app.use(express.json())
 
-app.post("/users", (request, response) => {
-  const { name, email, password } = request.body;
-
-
-  response.send(`Usuario: ${name}. - E-mail: ${email}. E a senha e: ${password}`)
-})
+app.use(routes)
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is ronning on Port ${PORT}`))
+
