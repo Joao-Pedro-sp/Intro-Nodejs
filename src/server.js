e08e7dc4300f6b1d70f1244d3112593598955d8f
joao <jp_santana00@outlook.com>
2022-11-24 14:52:14 -0300

    metodo POST

--- src/server.js
+++ src/server.js
@@ -1,19 +1,13 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
-app.get("/message/:id/:user", (request, response) => {
-  const { id, user } = request.params;
+app.post("/users", (request, response) => {
+  const { name, email, password } = request.body;
 
-  response.send(`
-  Mensagem ID: ${id}.
-  Para o usuario: ${user}.`)
-})
 
-app.get("/users", (request, response) => {
-  const { page, limit } = request.query;
-
-  response.send(`Pagina: ${page}. Mostrar: ${limit}`)
+  response.send(`Usuario: ${name}. - E-mail: ${email}. E a senha e: ${password}`)
 })
 
 const PORT = 3333
