5347eecde2161b689986bef473cb98cd18cf045b
joao <jp_santana00@outlook.com>
2022-12-02 04:28:38 -0300

    add update user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -19,6 +19,32 @@ class UsersController {
 
     return response.status(201).json();
   }
+
+  async update(request, response) {
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    if(!user) {
+      throw new AppError("Usuario nao encontrado");
+    }
+
+    const userWithUpdateEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+    if(userWithUpdateEmail && userWithUpdateEmail.id !== user.id){
+      throw new AppError("este email ja esta em uso.");
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run(`UPDATE users SET name = ?, email = ?, updated_at = ?, WHERE id = ?`,
+    [user.name, user.email, new Date(), id]);
+
+    return response.json();
+  }
   /**
    * index - GET para listar varios registros.
    * show - GET para exibir um registro especifico.
